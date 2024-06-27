abd911614174f88bdabe6395afaee3c47f530299
江澤誠哉 <ezawaseiya@kousawaseiyanoMacBook-Pro.local>
2024-06-27 22:44:18 +0900

    prepare screen base

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,26 @@
 "use client";
-import { Amplify, Auth } from "aws-amplify";
+import { Amplify } from "aws-amplify";
 import { withAuthenticator } from "@aws-amplify/ui-react";
 import Image from "next/image";
 import styles from "./page.module.css";
 import { useAuthenticator } from "@aws-amplify/ui-react"; //AmplifyでReactのuiを提供するライブラリ
 import "@aws-amplify/ui-react/styles.css"; //css適用
 import * as AmazonCognitoIdentity from "amazon-cognito-identity-js";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import CssBaseline from "@mui/material/CssBaseline";
+import AppBar from "@mui/material/AppBar";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import MenuItem from "@mui/material/MenuItem";
+import { useLoginUser } from "./hooks/useLoginUser";
+import BeachAccessIcon from "@mui/icons-material/BeachAccess";
+import CoffeeIcon from "@mui/icons-material/Coffee";
 
 Amplify.configure({
   Auth: {
@@ -17,131 +32,72 @@ Amplify.configure({
 });
 function Home() {
   const { user, route } = useAuthenticator((context) => [context.user]);
-  const poolData = {
-    UserPoolId: "ap-northeast-1_llO4mR1XR",
-    ClientId: "570fo2tn3km1dmmsl6haa94l0k",
+  const { item } = useLoginUser();
+  const drawerWidth = 240;
+  type MenuItem = {
+    name: string;
+    url: string;
+    icon: React.ReactNode;
   };
-  const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
-  const cognitoUser = userPool.getCurrentUser();
-  console.log(cognitoUser);
-  let currentUserData = {};
-  const getUserAttribute = () => {
-    // 現在のユーザー情報が取得できているか？
-    if (cognitoUser != null) {
-      cognitoUser.getSession(function (err, session) {
-        if (err) {
-          console.log(err);
-        } else {
-          // ユーザの属性を取得
-          cognitoUser.getUserAttributes(function (err, result) {
-            if (err) {
-              console.log("エラーです" + err);
-            }
-
-            // 取得した属性情報を連想配列に格納
-            for (let i = 0; i < result.length; i++) {
-              currentUserData[result[i].getName()] = result[i].getValue();
-            }
-            console.log(currentUserData);
-          });
-        }
-      });
-    } else {
-      console.log("elseに入ったよ");
-    }
-  };
-
+  const menuList: MenuItem[] = [
+    { name: "マイページ", url: "/page1", icon: <BeachAccessIcon /> },
+    { name: "ダッシュボード", url: "/page2", icon: <CoffeeIcon /> },
+  ];
   return (
-    <main className={styles.main}>
-      <button onClick={() => cognitoUser.signOut()}>Sign Out</button>
-      <button onClick={() => getUserAttribute()}>Sign Out</button>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    // <main className={styles.main}>
+    // <button onClick={() => cognitoUser.signOut()}>Sign Out</button>
+    // <button onClick={() => getUserAttribute()}>Sign Out</button>
+    /* <div className={styles.description}></div> */
+    /* </main> */
+    // <html lang="en">
+    // <body className={inter.className}>
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      >
+        <Toolbar>
+          <Typography variant="h6" noWrap component="div">
+            <Link href="/" underline="none" color="inherit">
+              MUI 管理画面
+            </Link>
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant="permanent"
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          [`& .MuiDrawer-paper`]: {
+            width: drawerWidth,
+            boxSizing: "border-box",
+          },
+        }}
+      >
+        <Toolbar />
+        <Box sx={{ overflow: "auto" }}>
+          <List>
+            {menuList.map(({ name, url, icon }: MenuItem) => (
+              <ListItem key={name} disablePadding>
+                <ListItemButton selected={console.log(name)}>
+                  <ListItemIcon>{icon}</ListItemIcon>
+                  <Link href={url} underline="none" color="inherit">
+                    {name}
+                  </Link>
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Drawer>
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <Toolbar />
+      </Box>
+    </Box>
+    // </body>
+    // </html>
   );
 }
 
