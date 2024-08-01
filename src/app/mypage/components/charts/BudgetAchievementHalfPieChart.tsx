f1bc29b205ae263d6fbd80cbc62a3731cab22e49
江澤誠哉 <ezawaseiya@kousawaseiyanoMacBook-Pro.local>
2024-08-02 08:33:21 +0900

    fix to deploy2

--- src/app/mypage/components/charts/BudgetAchievementHalfPieChart.tsx
+++ src/app/mypage/components/charts/BudgetAchievementHalfPieChart.tsx
@@ -27,42 +27,42 @@ const BudgetAchievementHalfPieChart: FC<Props> = ({ values }) => {
   const iR = 50;
   const oR = 70;
 
-  const needle = (
-    value: number,
-    data: any[],
-    cx: number,
-    cy: number,
-    iR: number,
-    oR: number,
-    color: string
-  ) => {
-    let total = 0;
-    data.forEach((v) => {
-      total += v.value;
-    });
-    const ang = 180.0 * (1 - value / total);
-    const length = (iR + 2 * oR) / 3;
-    const sin = Math.sin(-RADIAN * ang);
-    const cos = Math.cos(-RADIAN * ang);
-    const r = 5;
-    const x0 = cx + 5;
-    const y0 = cy + 5;
-    const xba = x0 + r * sin;
-    const yba = y0 - r * cos;
-    const xbb = x0 - r * sin;
-    const ybb = y0 + r * cos;
-    const xp = x0 + length * cos;
-    const yp = y0 + length * sin;
+  // const needle = (
+  //   value: number,
+  //   data: any[],
+  //   cx: number,
+  //   cy: number,
+  //   iR: number,
+  //   oR: number,
+  //   color: string
+  // ) => {
+  //   let total = 0;
+  //   data.forEach((v) => {
+  //     total += v.value;
+  //   });
+  //   const ang = 180.0 * (1 - value / total);
+  //   const length = (iR + 2 * oR) / 3;
+  //   const sin = Math.sin(-RADIAN * ang);
+  //   const cos = Math.cos(-RADIAN * ang);
+  //   const r = 5;
+  //   const x0 = cx + 5;
+  //   const y0 = cy + 5;
+  //   const xba = x0 + r * sin;
+  //   const yba = y0 - r * cos;
+  //   const xbb = x0 - r * sin;
+  //   const ybb = y0 + r * cos;
+  //   const xp = x0 + length * cos;
+  //   const yp = y0 + length * sin;
 
-    return [
-      <circle cx={x0} cy={y0} r={r} fill={color} stroke="none" />,
-      <path
-        d={`M${xba} ${yba}L${xbb} ${ybb} L${xp} ${yp} L${xba} ${yba}`}
-        stroke="#none"
-        fill={color}
-      />,
-    ];
-  };
+  //   return [
+  //     <circle cx={x0} cy={y0} r={r} fill={color} stroke="none" />,
+  //     <path
+  //       d={`M${xba} ${yba}L${xbb} ${ybb} L${xp} ${yp} L${xba} ${yba}`}
+  //       stroke="#none"
+  //       fill={color}
+  //     />,
+  //   ];
+  // };
   const renderCustomizedLabel = ({
     cx,
     cy,
@@ -86,6 +86,7 @@ const BudgetAchievementHalfPieChart: FC<Props> = ({ values }) => {
 
     return (
       <text
+        key={`label-text-${index}`}
         x={x}
         y={y}
         fill={grey[800]}
